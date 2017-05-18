8e9c5d4108d49040867e81775dbe79a1311f7fb2
k-kurikuri <kurimixgoo@gmail.com>
2017-05-18 23:49:18 +0900

    change : 日本語 => 英語へと文言を修正

--- resources/views/twitter/index.blade.php
+++ resources/views/twitter/index.blade.php
@@ -1,33 +1,34 @@
 @extends('layout.app')
 
-@section('title', 'ツイート一覧')
+@section('title', 'user tweet list')
 
 @section('content')
-    <h2 id="table-head-options">Tweet一覧 (一度に200件表示します)</h2>
+    <h2 id="table-head-options">User Tweets</h2>
 
     <form method="post" action="/twitter/remove">
         {{ csrf_field() }}
         <div class="form-group">
-        <table class="table">
-          <thead class="thead-inverse">
-            <tr>
-              <th>tweetId</th>
-              <th>text</th>
-              <th>#</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach($timeLines as $timeLine)
-            <tr>
-              <th scope="row">{{ $timeLine->id }}</th>
-              <td>{{ $timeLine->text }}</td>
-              <td>
-                  <input type="checkbox" name="tweetIds[]" value="{{ $timeLine->id }}" class="form-check-input" />
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-        <button type="submit" class="btn btn-primary">ツイート削除</button>
+            <table class="table">
+              <thead class="thead-inverse">
+                <tr>
+                  <th>tweetId</th>
+                  <th>text</th>
+                  <th>#</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($timeLines as $timeLine)
+                <tr>
+                  <th scope="row">{{ $timeLine->id }}</th>
+                  <td>{{ $timeLine->text }}</td>
+                  <td>
+                      <input type="checkbox" name="tweetIds[]" value="{{ $timeLine->id }}" class="form-check-input" />
+                  </td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+        <button type="submit" class="btn btn-primary">remove check tweet</button>
+        </div>
     </form>
 @endsection
